7f8b46c6f030be67e346818db2635e5d0f31f131
Gabriel Pérez <gabrielprzb@gmail.com>
2019-11-14 20:49:00 -0600

    oas

---
+++ schemas.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const URL_MONGO = "mongodb+srv://cyn2903:1234567.@cluster0-iqsf0.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(URL_MONGO,{ useNewUrlParser:true },(err) => {
+    if(!err){ 
+        console.log('Conexión exitosa en MongoDB')
+    }else{
+        console.log(err)
+    };
+    
+});
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+    name: String,
+    profilePicture: String,
+    rating: Number,
+    city: String,
+    cp: Number,
+    email: String,
+    password: String
+
+},{ timestamps:true });
+
+const dressSchema = new Schema({
+
+    user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    picture: String,
+    size: String,
+    type: String, //coctel, etiquite, etc
+    color: String,
+    brand: String,
+    description: String,
+    availability: {type: Boolean, default: false},
+    price: Number,
+    warranty: Number //Por si se daña
+
+
+},{ timestamps:true });
+
+
+const orderSchema = new Schema({
+
+    dress: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Dress'
+    },
+    user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    returnDate: Date,
+    getDate: Date
+
+},{ timestamps: true });
+
+const User = mongoose.model( 'User', userSchema);
+const Dress = mongoose.model( 'Dress', dressSchema);
+const Order = mongoose.model( 'Order', orderSchema);
+
+module.exports = {
+    User,
+    Dress,
+    Order
+}
---
+++ server.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const { User, Dress, Order } = require('./schemas');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server has been initialized on port:${PORT}`)
+});
+
+
+app.post('/user/create', (req, res) => {
+
+    const {
+        name,
+        profilePicture,
+        rating,
+        city,
+        cp,
+        email,
+        password
+    } = req.body;
+
+    const newUser = User({
+        name,
+        profilePicture,
+        rating,
+        city,
+        cp,
+        email,
+        password
+    })
+
+    newUser.save((err, user) => {
+
+        if (err) res.status(409).send({ message: 'Hubo un error al crear el usuario', error: err })
+        else {
+            res.status(201).send({ message: 'Usuario creado con éxito', usuario: user })
+        }
+
+    })
+
+})
+
+app.post('/dress/create', (req, res) => {
+
+    const {
+        picture,
+        user,
+        size,
+        type, //coctel, etiquite, etc
+        color,
+        brand,
+        description,
+        availability,
+        price,
+        warranty //Por si se daña
+    } = req.body;
+
+
+    const newDress = Dress({
+        picture,
+        user,
+        size,
+        type, //coctel, etiquite, etc
+        color,
+        brand,
+        description,
+        availability,
+        price,
+        warranty
+    })
+
+    newDress.save((err, dress) => {
+
+        if (err) res.status(409).send({ message: 'Hubo un error al crear el vestido', error: err })
+        else {
+            res.status(201).send({ message: 'Vestido creado con éxito', dress: dress })
+        }
+
+    })
+
+})
+
+app.post('/order/create', (req, res) => {
+
+    const {
+        dress,
+        user,
+        returnDate,
+        getDate
+    } = req.body;
+
+
+    const newOrder = Order({
+        dress,
+        user,
+        returnDate,
+        getDate
+    })
+
+    newOrder.save((err, order) => {
+
+        if (err) res.status(409).send({ message: 'Hubo un error al crear el pedido', error: err })
+        else {
+            res.status(201).send({ message: 'Pedido creado con éxito', order: order })
+        }
+
+    })
+
+})
+
+
+app.get('/dress', (req, res) => { // donde se jalan todos los vestidos
+
+    let userId = req.query.userId;
+    Dress.find({ user: userId })
+        .exec((err, dresses) => {
+            if (err) res.status(409).send({ dresses: null, error: err })
+            else {
+                res.status(201).send({ dresses: dresses })
+            }
+
+        })
+})
+
+app.get('/order', (req, res) => { // donde se jalan todos los vestidos
+    Order.find({})
+        .exec((err, orders) => {
+            if (err) res.status(409).send({ orders: null, error: err })
+            else {
+                res.status(201).send({ orders: orders })
+            }
+
+        })
+})
